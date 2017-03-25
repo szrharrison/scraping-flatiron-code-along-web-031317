c1d9b039d7aa4192514ff037053517d54b750693
szrharrison <s.z.r.harrison@gmail.com>
2017-03-25 17:49:34 -0400

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,16 @@
+class Course
+  def initialize
+    ALL << self
+  end
+  attr_accessor :title, :schedule, :description
+  ##################################################
+  ALL = []
 
+  def self.all
+    ALL
+  end
 
+  def self.reset_all
+    ALL.clear
+  end
+end
